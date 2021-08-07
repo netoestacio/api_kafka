6a7de997853e972310cfd15c1ab226a17245d8d6
Pedro Estacio <pedroestacio@MacBook-Pro-de-Pedro.local>
2021-08-06 22:44:26 -0300

    Inicial

---
+++ src/test/java/br/com/kafkaApi/KafkaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kafkaApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
